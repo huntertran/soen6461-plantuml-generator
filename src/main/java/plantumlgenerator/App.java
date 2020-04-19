61814235d3d84fe09256373b1a19ffc15df0b310
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-04-18 22:15:24 -0400

    add padl.jar

--- src/main/java/plantumlgenerator/App.java
+++ src/main/java/plantumlgenerator/App.java
@@ -1,13 +1,12 @@
 package plantumlgenerator;
 
-/**
- * Hello world!
- *
- */
+import padl.kernel.IFirstClassEntity;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        IFirstClassEntity firstClassEntity;
     }
 }
