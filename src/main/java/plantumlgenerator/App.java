5f405f91e7e7c5f8c1243e550fff982c931a77b4
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-04-29 15:03:03 -0400

    show result window

--- src/main/java/plantumlgenerator/App.java
+++ src/main/java/plantumlgenerator/App.java
@@ -1,17 +1,37 @@
 package plantumlgenerator;
 
+import java.awt.TextArea;
+
+import javax.swing.JFrame;
+
 import padl.kernel.ICodeLevelModel;
 import plantumlgenerator.utils.MetaModelCreator;
 import plantumlgenerator.visitor.PlantUmlGenerator;
-import plantumlgenerator.visitor.PlantUmlWalker;
 
 public class App {
     public static void main(String[] args) {
         ICodeLevelModel models = MetaModelCreator.createMetaModelsFromCompiledClasses("./src/test/java/plantumlgenerator/padl/event/");
-        
-//        String walked = (String) models.walk(new PlantUmlWalker());
+
+        // String walked = (String) models.walk(new PlantUmlWalker());
         String generatedString = models.generate(new PlantUmlGenerator());
-        
+
         System.out.println(generatedString);
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                creatAndShowUI(generatedString);
+            }
+        });
+
+    }
+
+    private static void creatAndShowUI(String generatedUmlString) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Generated Plantuml code");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new TextArea(generatedUmlString));
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
