ed064e62475065d3f01bb5f4721eac04fe3fc89e
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-04-30 15:04:41 -0400

    java 1.7

--- src/main/java/plantumlgenerator/App.java
+++ src/main/java/plantumlgenerator/App.java
@@ -18,7 +18,7 @@ public class App {
      * @param args[0] the folder contain compiled java classes
      * @param args[1] is show the result in console window. Accept "y" for yes and "n" for no
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         String compiledClassesFolder = "";
         boolean isShowResultInConsole = false;
@@ -26,14 +26,14 @@ public class App {
         if (args.length != 0) {
             compiledClassesFolder = args[0];
         } else {
-            Scanner scanner = new Scanner(System.in);
+            final Scanner scanner = new Scanner(System.in);
             System.out.print("Enter the folder contain compiled java classes: ");
             compiledClassesFolder = scanner.nextLine();
-            
+
             System.out.print("Show the result in console windows? [y] yes - [n] no: ");
-            String yesNo = scanner.nextLine();
-            
-            switch(yesNo.toLowerCase()) {
+            final String yesNo = scanner.nextLine();
+
+            switch (yesNo.toLowerCase()) {
                 case "yes":
                 case "y": {
                     isShowResultInConsole = true;
@@ -44,30 +44,30 @@ public class App {
                     break;
                 }
             }
-            
+
             scanner.close();
         }
 
         // compiledClassesFolder = "./src/test/java/plantumlgenerator/padl/event/";
-        ICodeLevelModel models = MetaModelCreator.getInstance()
-                                                 .createMetaModelsFromCompiledClasses(compiledClassesFolder);
+        final ICodeLevelModel models = MetaModelCreator.getInstance()
+                .createMetaModelsFromCompiledClasses(compiledClassesFolder);
 
         // String walked = (String) models.walk(new PlantUmlWalker());
-        String generatedString = models.generate(new PlantUmlGenerator());
+        final String generatedString = models.generate(new PlantUmlGenerator());
 
         if (isShowResultInConsole) {
             System.out.println(generatedString);
         } else {
             javax.swing.SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
-                    creatAndShowUI(generatedString);
+                    createAndShowUI(generatedString);
                 }
             });
         }
     }
 
-    private static void creatAndShowUI(String generatedUmlString) {
-        JFrame frame = new JFrame();
+    private static void createAndShowUI(final String generatedUmlString) {
+        final JFrame frame = new JFrame();
         frame.setTitle("Generated Plantuml code");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(new TextArea(generatedUmlString));
